1e92234e9139e2678853e38152a9f6051c78ca8c
strosu <rosu@outlook.com>
2019-01-20 10:56:46 +0200

    corrent logic for calculating amounts

--- EveIndustryStandard/Managers/BlueprintManager.cs
+++ EveIndustryStandard/Managers/BlueprintManager.cs
@@ -15,6 +15,52 @@ namespace EveIndustryStandard.Managers
             _bpcs = GetBlueprints();
         }
 
+        public List<Component> GetComponents(int itemId, int amount, double bpcMaterialModifier, BlueprintCopy bpc)
+        {
+            var requiredComponents = new List<Component>();
+
+            var fullRuns = amount / bpc.MaxRuns;
+            var runsPartial = amount % bpc.MaxRuns;
+
+            for (int i = 0; i < fullRuns; i++)
+            {
+                AddComponents(requiredComponents, ConcreteGetComponents(itemId, bpc.MaxRuns, bpcMaterialModifier, bpc));
+            }
+
+            AddComponents(requiredComponents, ConcreteGetComponents(itemId, runsPartial, bpcMaterialModifier, bpc));
+
+            return requiredComponents;
+        }
+
+        private void AddComponents(List<Component> previous, List<Component> current)
+        {
+            foreach (var comp in current)
+            {
+                var prev = previous.FirstOrDefault(x => x.Id == comp.Id);
+                if (prev == null)
+                {
+                    previous.Add(comp);
+                    continue;
+                }
+
+                prev.Amount += comp.Amount;
+            }
+        }
+
+        private List<Component> ConcreteGetComponents(int itemId, int amount, double bpcMaterialModifier, BlueprintCopy bpc)
+        {
+            var multiplier = 0.853578;
+            var baseComponents = bpc.UnresearchedRequiredComponentsForSingleRun.ToList();
+            foreach (var comp in baseComponents)
+            {
+                comp.Amount = (int)Math.Max(amount, Math.Ceiling(Math.Round(amount * comp.Amount * bpcMaterialModifier * multiplier, 2)));
+            }
+
+            return baseComponents;
+        }
+
+        // Take itemId => bpc => List<Component> needed => send back to ItemBUilder
+        // maybe also take the efficiencies here
         public static Dictionary<int, BlueprintCopy> GetBlueprints()
         {
             var result = new Dictionary<int, BlueprintCopy>();
@@ -82,7 +128,7 @@ namespace EveIndustryStandard.Managers
 
         private int GetRequiredComponents(int bpcId, int componentTypeId)
         {
-            var multiplier = 0.853578;
+            var multiplier = 0.853578; // should be the citadel effect
             return (int)Math.Ceiling(_bpcs[bpcId].UnresearchedRequiredComponentsForSingleRun.FirstOrDefault(x => x.Id == componentTypeId).Amount * multiplier);
         }
     }
